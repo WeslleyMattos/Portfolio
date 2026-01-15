82dd33462a3cfa2b94d3a57a931e1bc5f71861d6
Weslley <paocomovoxs@hotmail.com>
2026-01-14 21:56:58 -0300

    generate

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,6 +4,9 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   vite: {
     plugins: [tailwindcss()],
+    build: {
+      sourcemap: false,
+    },
   },
   modules: [
     '@nuxt/image',
